a4f1e5acca08d1b7e85a7d70ca18cc0a3ac35f72
sohnryang <loop.infinitely@gmail.com>
2018-02-27 17:10:23 +0900

    초기 커밋.

---
+++ EmbeddedWindow/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System;
+
+namespace EmbeddedWindow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hwndChild, IntPtr hwndNewParent);
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Process p = Process.Start("notepad.exe");
+            p.WaitForInputIdle();
+            SetParent(p.MainWindowHandle, this.Handle);
+        }
+    }
+}
